a1faef6cca9f8ff81d7ba3e22e3ad5c5b9f9a6c7
shlevanto <simo.levanto@helsinki.fi>
2022-04-29 16:38:00 +0300

    Primin algoritmi toimii

--- prim/app/Main.hs
+++ prim/app/Main.hs
@@ -1,15 +1,19 @@
 module Main where
 
 import Prim
+import Kaari
 
 main :: IO ()
 main = do
-  let s1 = Kaari 1 2 2 100
-  let s2 = Kaari 1 3 101 101
-  let s3 = Kaari 2 3 2 102
+  let s1 = Kaari 1 2 2 12
+  let s2 = Kaari 1 3 4 14
+  let s3 = Kaari 2 3 1 23
+  let s4 = Kaari 2 4 2 24
+  let s5 = Kaari 3 5 7 35
+  let s6 = Kaari 4 5 5 45
   
-  let g = [s1, s2, s3]
+  let g = [s1, s2, s3, s4, s5, s6]
   
-  putStrLn $ show (alku $ head g)
+  -- putStrLn $ show (keko g)
   putStrLn $ show (prim g)
 
--- prim/src/Prim.hs
+++ prim/src/Prim.hs
@@ -3,42 +3,14 @@ module Prim where
 import Data.Heap
 import Data.Set
 
--- Kaari on tietorakenne, jolla on alku, loppu ja paino
-data Kaari = Kaari Int Int Int Int
-  deriving (Show, Eq)
-
-instance Ord Kaari where
-  Kaari a1 l1 p1 id1 <= Kaari a2 l2 p2 id2
-    | p1 < p2     = True
-    | p1 > p2     = False
-    | otherwise   = p1 == p2  
-
-alku :: Kaari -> Int
-alku (Kaari a l p id) = a
-
-loppu :: Kaari -> Int
-loppu (Kaari a l p id) = l
-
-paino :: Kaari -> Int
-paino (Kaari a l p id) = p
-
-kaariId :: Kaari -> Int
-kaariId (Kaari a l p id) = id
-
-vastaKaari :: Kaari -> Kaari
-vastaKaari (Kaari a l p id) = Kaari l a p id
-
--- pienin virittävä puu
--- 1. Funktio ottaa annetun juuren ja lisää sen puulistaukseen
--- 2. Lisätään kaikki juuren kaaret kekoon
--- 3. Aina keon ensimmäiselle kaarelle: jos molemmat kaaren solmut puussa -> skippaa, jos toinen -> lisää puuhun
+import Kaari
 
 keko :: [Kaari] -> MinHeap Kaari
 keko g = Data.Heap.fromList g :: MinHeap Kaari
 
 tutki :: MinHeap Kaari -> [Kaari] -> Set Int -> [Kaari]
 tutki keko puu solmut
-  | keko == Data.Heap.empty = puu 
+  | keko == Data.Heap.empty = puu -- ok
   | member (alku (poll keko)) solmut &&  notMember (loppu (poll keko)) solmut = tutki (Data.Heap.drop 1 keko) (puu ++ [poll keko]) (Data.Set.insert (loppu (poll keko)) solmut)
   | member (loppu (poll keko)) solmut &&  notMember (alku (poll keko)) solmut = tutki (Data.Heap.drop 1 keko) (puu ++ [poll keko]) (Data.Set.insert (alku (poll keko)) solmut)
   | otherwise = tutki (Data.Heap.drop 1 keko) (puu) (solmut)
@@ -48,4 +20,4 @@ poll :: MinHeap Kaari -> Kaari
 poll keko = head $ Data.Heap.take 1 keko
 
 prim :: [Kaari] -> [Kaari]
-prim g = tutki (keko g) [] (Data.Set.fromList [(alku $ head g)])
+prim g = tutki (keko g) [] (Data.Set.fromList [alku $ poll (keko g)])
