8133710c26b3704e54f202587f06539cf8528e81
shlevanto <simo.levanto@helsinki.fi>
2022-04-29 16:38:20 +0300

    Tänne tulee JSON parseri.

--- prim/src/Parser.hs
+++ prim/src/Parser.hs
@@ -2,9 +2,9 @@ module Parser where
 
 import Data.Aeson
 
-data Node = Node {id :: Int, xloc :: Int, yloc :: Int, edges :: [(Int, Int)]}
+--data Node = Node {id :: Int, xloc :: Int, yloc :: Int, edges :: [(Int, Int)]}
 
-instance FromJSON Node
+--instance FromJSON Node
 
-foo :: Object v -> Node
-foo v = 
+foo :: Int -> Int
+foo v = v
